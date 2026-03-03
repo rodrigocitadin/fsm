18aa30029fda26d442be670885f9dd3c1da61577
rodrigocitadin <citadinrodrigo@gmail.com>
2026-03-02 23:47:35 -0300

    flat and graph versions

---
+++ flat/fsm.go
@@ -0,0 +1,57 @@
+package flat
+
+import "fmt"
+
+type State string
+type Event string
+
+type StateEvent struct {
+	State State
+	Event Event
+}
+
+type Transition struct {
+	NextState State
+	Rules     []func() bool
+}
+
+type FiniteStateMachine struct {
+	CurrentState State
+	Table        map[StateEvent]Transition
+}
+
+func NewFSM(initial State, table map[StateEvent]Transition) *FiniteStateMachine {
+	return &FiniteStateMachine{
+		CurrentState: initial,
+		Table:        table,
+	}
+}
+
+func (fsm *FiniteStateMachine) Compute(events []Event) {
+	for _, event := range events {
+		fmt.Printf("Processing event: %s\n", event)
+
+		key := StateEvent{State: fsm.CurrentState, Event: event}
+
+		transition, exists := fsm.Table[key]
+		if !exists {
+			fmt.Printf("  -> Ignored: Nothing happens in %s when %s occurs\n", fsm.CurrentState, event)
+			continue
+		}
+
+		canTransition := true
+		for _, ruleFn := range transition.Rules {
+			if !ruleFn() {
+				canTransition = false
+				break
+			}
+		}
+
+		if canTransition {
+			fmt.Printf("  -> Success: Transitioning %s to %s\n", fsm.CurrentState, transition.NextState)
+			fsm.CurrentState = transition.NextState
+		} else {
+			fmt.Printf("  -> Blocked: Rule prevented transition\n")
+		}
+	}
+}
---
+++ graph/fsm.go
@@ -0,0 +1,59 @@
+package graph
+
+import "fmt"
+
+type Event string
+
+type State struct {
+	Value  string
+	Arrows []Arrow
+}
+
+type Arrow struct {
+	Edge  *State
+	Event Event
+	Rules []func() bool
+}
+
+type FiniteStateMachine struct {
+	CurrentState *State
+}
+
+func NewFiniteStateMachine(initialState *State) *FiniteStateMachine {
+	return &FiniteStateMachine{
+		CurrentState: initialState,
+	}
+}
+
+func (fsm *FiniteStateMachine) Compute(events []Event) {
+	for _, event := range events {
+		fmt.Printf("Processing event: %s\n", event)
+
+		transitioned := false
+
+		for _, arrow := range fsm.CurrentState.Arrows {
+			if event == arrow.Event {
+				canTransition := true
+				for _, ruleFn := range arrow.Rules {
+					if !ruleFn() {
+						canTransition = false
+						break
+					}
+				}
+
+				if canTransition {
+					fmt.Printf("  -> Success: Transitioning %s to %s\n", fsm.CurrentState.Value, arrow.Edge.Value)
+					fsm.CurrentState = arrow.Edge
+					transitioned = true
+					break
+				} else {
+					fmt.Printf("  -> Blocked: Rule prevented transition\n")
+				}
+			}
+		}
+
+		if !transitioned {
+			fmt.Printf("  -> Ignored: Nothing happens in %s when %s occurs\n", fsm.CurrentState.Value, event)
+		}
+	}
+}
